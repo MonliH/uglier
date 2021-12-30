a6d387eb5ebbf47512ff98692a6884af3af0ce05
Jonathan Li <30177086+MonliH@users.noreply.github.com>
2021-12-29 22:20:44 -0500

    Make renaming work generically

--- uglier/__main__.py
+++ uglier/__main__.py
@@ -1,10 +1,13 @@
-from typing import Tuple
 from rope.base.project import Project
 from rope.base.libutils import analyze_modules
-import ast
-
-from rope.refactor.extract import ExtractVariable
 from rope.refactor.rename import Rename
+from rope.base.exceptions import RefactoringError
+
+import ast
+import logging
+
+logger = logging.getLogger("uglier")
+logger.setLevel(logging.WARNING)
 
 
 def coords_to_index(line: int, col: int, s: str):
@@ -12,17 +15,36 @@ def coords_to_index(line: int, col: int, s: str):
     return sum(len(line) + 1 for line in s.splitlines()[: line - 1]) + col
 
 
+def make_new(old_id: str):
+    """Create a new variable name."""
+    return "new_" + old_id
+
+
 project = Project("./test/")
 analysis = analyze_modules(project)
+builtins = set(dir(__builtins__))
+
 for file in project.get_files():
-    print(dir(file))
-    # project.do(changes)
-    file_s = file.read()
-    file_ast = ast.parse(file_s)
-    for node in ast.walk(file_ast):
-        if isinstance(node, ast.Name):
-            idx = coords_to_index(node.lineno, node.col_offset, file_s)
-            print(node.id, file_s[idx : idx + len(node.id)])
-            changes = ExtractVariable(
-                project, file, idx, idx + len(node.id)
-            ).get_changes(f"new_{node.id}")
+    changes = []
+    changed_vars = set()
+
+    while True:
+        file_s = file.read()
+        file_ast = ast.parse(file_s)
+        did_something = False
+        for node in ast.walk(file_ast):
+            if isinstance(node, ast.Name):
+                if node.id not in builtins and node.id not in changed_vars:
+                    did_something = True
+                    idx = coords_to_index(node.lineno, node.col_offset, file_s)
+                    new_id = make_new(node.id)
+                    try:
+                        change = Rename(project, file, idx).get_changes(new_id)
+                        project.do(change)
+                    except RefactoringError as e:
+                        logger.log(logging.WARNING, str(e))
+                    changed_vars.add(new_id)
+                    break
+
+        if not did_something:
+            break
